a73106656a4bcd12179dbf0b811919e6a4c5de40
rati234 <bkhandamashvili@gmail.com>
2025-03-29 01:19:39 +0400

    commit

--- day 14/homework/lesson.py
+++ day 14/homework/lesson.py
@@ -1,11 +1,49 @@
-name = "Rati"
+# name = "Rati"
 
-vowels = "a, e, i, o, u," 
+# vowels = "a, e, i, o, u," 
 
-vowels_letters = [letter for letter in name if letter in vowels] 
+# vowels_letters = [letter for letter in name if letter in vowels] 
 
 
-if vowels_letters:
-    print("vowels", " ". join(vowels_letters))
+# if vowels_letters:
+#     print("vowels", " ". join(vowels_letters))
+# else:
+#     print("names didnt found in vowels") 
+
+
+#სტრინგები უცვლელია (immutable)  რადგან როცა სტრინგს ქმნი ის მეხსიერებაში ინახება ისე რომ მისი ცალკეული სიმბოლოები შეცვლადი არარის 
+
+
+#სიები (LIST) ცვლადია რადგან სია არის ისეთი მონაცემთა სტრუქტურა სადაც ელემენტების პირდაპირი ცვლილება შეგვიძლია 
+
+
+# list = [] 
+
+# for _ in  range (8):
+#     value = input("please enter your name and surname: ")
+#     value = input("age: ")
+#     value = input("date of birth: ") 
+#     value = input("height and your living adress: ")
+#     value = input("what sport you like and whats your job: ") 
+#     list.append(value) 
+# print("your input is:", list) 
+
+
+
+numbers = []  
+
+for _ in range(10):
+    meaning = int(input("please enter a meaning: ")) 
+    numbers.append(meaning) 
+print("your entered information") 
+
+for num in numbers: 
+    if num % 2 == 0: 
+        print(f"{num} - evem") 
 else:
-    print("names didnt found in vowels") 
+    print(f"{num} - odd")   
+
+
+
+
+
---
+++ day 15/homework/lesson.py
@@ -0,0 +1,18 @@
+sentence  = input("please enter a sentence: ")
+
+
+lowercase = sentence.lower() 
+
+
+capitalize = sentence.capitalize() 
+
+
+uppercase = sentence.upper()  
+
+
+
+print("\nFormatted sentences:")
+print("1. ALL lowercase: ", lowercase) 
+print("2. First letter uppercase, rest lowercase: ", capitalize) 
+print("3. All  uppercase: ", uppercase) 
+
